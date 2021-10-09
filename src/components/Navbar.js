d6500a1f4a0df7c8540a59d7da38817dba77c09d
ncougs <ncougan@gmail.com>
2021-10-10 10:05:54 +1100

    Responsive behaviour for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const NavBar = ({ selectSection }) => {
 	return (
-		<Navbar bg='light' variant='light'>
+		<Navbar bg='light' variant='light' expand='lg'>
 			<Container>
 				<Navbar.Brand
 					onClick={() => {
@@ -12,36 +12,39 @@ const NavBar = ({ selectSection }) => {
 				>
 					React Portfolio
 				</Navbar.Brand>
-				<Nav className='me-auto'>
-					<Nav.Link
-						onClick={() => {
-							selectSection('About Me');
-						}}
-					>
-						About Me
-					</Nav.Link>
-					<Nav.Link
-						onClick={() => {
-							selectSection('Portfolio');
-						}}
-					>
-						Portfolio
-					</Nav.Link>
-					<Nav.Link
-						onClick={() => {
-							selectSection('Contact');
-						}}
-					>
-						Contact
-					</Nav.Link>
-					<Nav.Link
-						onClick={() => {
-							selectSection('Resume');
-						}}
-					>
-						Resume
-					</Nav.Link>
-				</Nav>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav' className='flex-grow-0'>
+					<Nav className='me-auto'>
+						<Nav.Link
+							onClick={() => {
+								selectSection('About Me');
+							}}
+						>
+							About Me
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								selectSection('Portfolio');
+							}}
+						>
+							Portfolio
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								selectSection('Contact');
+							}}
+						>
+							Contact
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								selectSection('Resume');
+							}}
+						>
+							Resume
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Container>
 		</Navbar>
 	);
